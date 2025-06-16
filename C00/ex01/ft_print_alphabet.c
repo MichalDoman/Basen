1abb4cf71a9b8e10a2a05066cc698343cc65c9a2
MichalDoman <mjd.domanski@gmail.com>
2025-06-16 15:37:02 +0200

    C00ex01 & ex02

---
+++ C00/ex01/ft_print_alphabet.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	char	c;
+	
+	c = '`';
+	while (c++ < 'z')
+		write(1, &c, 1);
+}
+
+int	main(void)
+{
+	ft_print_alphabet();
+	return (0);
+}
---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	c;
+	
+	c = '{';
+	while (c-- > 'a')
+		write(1, &c, 1);
+}
+
+int	main(void)
+{
+	ft_print_reverse_alphabet();
+	return (0);
+}
