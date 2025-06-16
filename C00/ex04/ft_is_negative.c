de1eb7dd9b561034bde51e778426c4dd63926ed0
MichalDoman <mjd.domanski@gmail.com>
2025-06-16 15:45:11 +0200

    C00ex04

---
+++ C00/ex04/ft_is_negative.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+void	ft_is_negative(int n)
+{
+	// write(1, (n < 0 ? "N" : "P"), 1);
+	if (n < 0)
+		write(1, "N", 1);
+	else
+		write(1, "P", 1);
+}
+
+int	main(void)
+{
+	ft_is_negative(-1);
+	ft_is_negative(0);
+	ft_is_negative(1);
+	return (0);
+}
