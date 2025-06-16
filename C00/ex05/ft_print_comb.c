0c39f6723e159852eb4babb7c6af65822f0f815e
MichalDoman <mjd.domanski@gmail.com>
2025-06-16 16:21:52 +0200

    C00ex05

---
+++ C00/ex05/ft_print_comb.c
@@ -0,0 +1,39 @@
+#include <unistd.h>
+
+void	ft_print_comb(void)
+{
+	char	i;
+	char	j;
+	char	k;
+	
+	i = '0';
+	while (i <= '7')
+	{
+		j = i + 1;
+		while(j <= '8')
+		{
+			k = j + 1;
+			while (k <= '9')
+			{
+				/*
+				Compound literal:
+					write(1, (char[]){i, j, k}, 3); 
+				*/
+				write(1, &i, 1);
+				write(1, &j, 1);
+				write(1, &k, 1);
+				if (i != '7')
+					write(1, ", ", 2);
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}		
+}
+
+int	main(void)
+{
+	ft_print_comb();
+	return (0);
+}
