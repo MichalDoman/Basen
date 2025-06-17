c501d3a390a658a0002cd53f45d1d0dbe4785602
MichalDoman <mjd.domanski@gmail.com>
2025-06-17 16:22:43 +0200

    C01ex02

---
+++ C01/ex02/ft_swap.c
@@ -0,0 +1,22 @@
+void	ft_swap(int *a, int *b)
+{
+	int	temp;
+	
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	int	a;
+	int	b;
+	
+	a = 3;
+	b = 12;
+	ft_swap(&a, &b);
+	printf("a = %d, b = %d", a, b);
+	return (0);
+}
