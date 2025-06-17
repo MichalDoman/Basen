357916dc3b74f1fc9ec23698574b41be0602973d
MichalDoman <mjd.domanski@gmail.com>
2025-06-17 17:45:09 +0200

    C01ex07

---
+++ C01/ex07/ft_rev_int_tab.c
@@ -0,0 +1,33 @@
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int	i;
+	int	temp;
+
+	i = 0;
+	while (i < size / 2)
+	{
+		temp = tab[i];
+		tab[i] = tab[size - 1 - i];
+		tab[size - 1 - i] = temp;
+		i++;
+	}
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	int	tab[] = {3, 1, 4, 1, 5, 9, 2, 6, 5};
+	int	size;
+	int	i;
+
+	size = 9;
+	ft_rev_int_tab(tab, size);
+	i = 0;
+	while (i < size)
+	{
+		printf("tab[%d] = %d \n", i, tab[i]);
+		i++;
+	}
+	return (0);
+}
