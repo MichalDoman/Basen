3cd64cef92c2b195d28290e1d90598fae6be2e25
MichalDoman <mjd.domanski@gmail.com>
2025-06-17 15:57:42 +0200

    C00ex08

---
+++ C00/ex08/ft_print_combn.c
@@ -0,0 +1,38 @@
+#include <unistd.h>
+
+void	handle_recursion(int n, int start, int depth, int *arr)
+{
+	int	val;
+	int	i;
+
+	val = start;
+	while (val <= 9)
+	{
+		arr[depth] = val;
+		if (depth + 1 < n)
+			handle_recursion(n, val + 1, depth + 1, arr);
+		else
+		{
+			i = -1;
+			while (i++ < n - 1)
+				write(1, (char []){'0' + arr[i]}, 1);
+			if (arr[0] != 10 - n)
+				write(1, ", ", 2);
+		}
+		val++;
+	}
+}
+
+void	ft_print_combn(int n)
+{
+	int	arr[10];
+
+	if (n > 0 && n < 10)
+		handle_recursion(n, 0, 0, arr);
+}
+
+int	main(void)
+{
+	ft_print_combn(4);
+	return (0);
+}
