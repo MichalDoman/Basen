06d0fbffa97608cd20e02e28a5c0232157125564
MichalDoman <mjd.domanski@gmail.com>
2025-06-22 18:23:24 +0200

    C02ex12

--- C02/ex12/ft_print_memory.c
+++ C02/ex12/ft_print_memory.c
@@ -1,18 +1,18 @@
 #include <unistd.h>
 
-void	print_address(unsigned long addr, int count)
+void	print_address(unsigned long addr, int depth)
 {
 	char	*hex;
 	
 	hex = "0123456789abcdef";
-	if (count < 16)
-		print_address(addr / 16, count + 1);
+	if (depth < 16)
+		print_address(addr / 16, depth + 1);
 	write(1, &hex[addr % 16], 1);
-	if (count == 1)
+	if (depth == 1)
 		write(1, ": ", 2);
 }
 
-void	print_hexes(unsigned char *str, int	start, int size)
+void	print_hexes(unsigned char *str, int line_len)
 {
 	int	i;
 	char	*hex;
@@ -21,11 +21,8 @@ void	print_hexes(unsigned char *str, int	start, int size)
 	i = 0;
 	while (i < 16)
 	{
-		if (i < size)
-		{
-			write(1, &hex[str[i + start] / 16], 1);
-			write(1, &hex[str[i + start] % 16], 1);
-		}
+		if (i < line_len)
+			write(1, (char []){hex[str[i] / 16], hex[str[i] % 16]}, 2);
 		else
 			write(1, "  ", 2);
 		if (i != 0 && (i + 1) % 2 == 0)
@@ -34,24 +31,37 @@ void	print_hexes(unsigned char *str, int	start, int size)
 	}
 }
 
+void	print_text(unsigned char *str, int line_len)
+{
+	int	i;
+	
+	i = 0;
+	while (i < line_len)
+	{
+		if (str[i] >= ' ' && str[i] <= '~')
+			write(1, &str[i], 1);
+		else
+			write(1, ".", 1);
+		i++;
+	}
+}
+
 void	*ft_print_memory(void *addr, unsigned int size)
 {
 	unsigned int	i;
-	unsigned int	size_left;
-	unsigned int	length;
+	unsigned int	line_len;
 
 	i = 0;
-	size_left = size;
-	length = 16;
+	line_len = 16;
 	while (i <= size)
 	{
-		if (size_left < 16)
-			length = size_left;
+		if (size - i < 16)
+			line_len = size - i;
 		print_address((unsigned long)(addr + i), 1);
-		print_hexes((unsigned char *)addr, i, length);
+		print_hexes((unsigned char *)(addr + i), line_len);
+		print_text((unsigned char *)(addr + i), line_len);
 		write(1, "\n", 1);
 		i += 16;
-		size_left -= length;
 	}
 	return (addr);
 }
