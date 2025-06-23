1005a167ce81c73bfd0283d68dfce55fb7dd7e0d
MichalDoman <mjd.domanski@gmail.com>
2025-06-23 11:19:48 +0200

    C03ex00

--- C03/ex00/ft_strcmp.c
+++ C03/ex00/ft_strcmp.c
@@ -1,15 +1,11 @@
 int	ft_strcmp(char *s1, char *s2)
 {
-	int	i;
-	
-	i = 0;
-	while (s1[i])
+	while (*s1 && (*s1 == *s2))
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
-		i++;
+		s1++;
+		s2++;
 	}
-	return (s1[i] - s2[i]);
+	return (*s1 - *s2);
 }
 
 #include <stdio.h>
@@ -20,8 +16,8 @@ int	main(void)
 	char *s1;
 	char *s2;
 	
-	s1 = "This is it";
-	s2 = "This is IT";
+	s1 = "This is ";
+	s2 = "This is";
 	printf("Original = %d \n", strcmp(s1, s2));
 	printf("My function = %d \n", ft_strcmp(s1, s2));
 	return (0);
