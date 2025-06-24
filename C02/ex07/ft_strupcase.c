3bee554045f1d5cf3ddec5b65b3014593e364fce
MichalDoman <mjd.domanski@gmail.com>
2025-06-24 15:37:21 +0200

    C02ex07

--- C02/ex07/ft_strupcase.c
+++ C02/ex07/ft_strupcase.c
@@ -1,27 +1,30 @@
 char	*ft_strupcase(char *str)
 {
 	int	i;
-	
-	i = 0;
-	while (str[i])
+
+	i = -1;
+	while (str[++i])
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
 			str[i] -= 32;
-		i++;
 	}
 	return (str);
 }
 
 #include <stdio.h>
+#include <string.h>
 
 int	main(void)
 {
-	char str_1[] = "What case is it?";
-	char str_2[] = "wHAT cASE iS tHIS?";
-	char str_3[] = "this is lowercase right?";
+	char	str_1[50];
+	char	str_2[50];
+	char	str_3[50];
 
-	printf("str_1 -> %s \n", ft_strupcase(str_1));
-	printf("str_2 -> %s \n", ft_strupcase(str_2));
-	printf("str_3 -> %s \n", ft_strupcase(str_3));
+	strcpy(str_1, "What case is it?");
+	strcpy(str_2, "wHAT cASE iS tHIS?");
+	strcpy(str_3, "this is lowercase right?");
+	printf("str_1 -> %s\n", ft_strupcase(str_1));
+	printf("str_2 -> %s\n", ft_strupcase(str_2));
+	printf("str_3 -> %s\n", ft_strupcase(str_3));
 	return (0);
 }
