2a85aeba7ca358e3d54e179fcfd20dbc46c6be33
MichalDoman <mjd.domanski@gmail.com>
2025-06-24 11:24:51 +0200

    C02ex00

--- C02/ex00/ft_strcpy.c
+++ C02/ex00/ft_strcpy.c
@@ -1,15 +1,11 @@
 char	*ft_strcpy(char *dest, char *src)
 {
 	int	i;
-	
-	i = 0;
-	while (src[i])
-	{
+
+	i = -1;
+	while (src[++i])
 		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
+	return (dest[i] = '\0', dest);
 }
 
 #include <stdio.h>
@@ -18,7 +14,7 @@ int	main(void)
 {
 	char	src[] = "To be copied, however it is pretty long";
 	char	dest[50] = "Nothing here";
-	
+
 	printf("dest = \"%s\"", ft_strcpy(dest, src));
 	return (0);
 }
