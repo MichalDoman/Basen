b68c5413fec1aa994b0ae531a9f1dfa74a2f91ec
MichalDoman <mjd.domanski@gmail.com>
2025-06-24 16:03:03 +0200

    C03ex02

--- C03/ex02/ft_strcat.c
+++ C03/ex02/ft_strcat.c
@@ -9,17 +9,18 @@ char	*ft_strcat(char *dest, char *src)
 	j = 0;
 	while (src[j])
 		dest[i++] = src[j++];
-	dest[i] = '\0';
-	return (dest);
+	return (dest[i] = '\0', dest);
 }
 
 #include <stdio.h>
+#include <string.h>
 
 int	main(void)
 {
-	char	dest[20] = "abc";
+	char	dest[20];
 	char	*src;
 
+	strcpy(dest, "abc");
 	src = "def";
 	printf("My function = %s \n", ft_strcat(dest, src));
 	return (0);
