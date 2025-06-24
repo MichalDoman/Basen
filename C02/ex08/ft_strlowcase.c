25ae111bc29e2e2052b0135d1e577dd9ace35b94
MichalDoman <mjd.domanski@gmail.com>
2025-06-24 15:38:48 +0200

    C02ex08

--- C02/ex08/ft_strlowcase.c
+++ C02/ex08/ft_strlowcase.c
@@ -2,24 +2,27 @@ char	*ft_strlowcase(char *str)
 {
 	int	i;
 	
-	i = 0;
-	while (str[i])
+	i = -1;
+	while (str[++i])
 	{
 		if (str[i] >= 'A' && str[i] <= 'Z')
 			str[i] += 32;
-		i++;
 	}
 	return (str);
 }
 
 #include <stdio.h>
+#include <string.h>
 
 int	main(void)
 {
-	char str_1[] = "What case is it?";
-	char str_2[] = "wHAT cASE iS tHIS?";
-	char str_3[] = "THIS IS UPPERCASE RIGHT?";
+	char	str_1[50];
+	char	str_2[50];
+	char	str_3[50];
 
+	strcpy(str_1, "What case is it?");
+	strcpy(str_2, "wHAT cASE iS tHIS?");
+	strcpy(str_3, "THIS IS UPPERCASE RIGHT?");
 	printf("str_1 -> %s \n", ft_strlowcase(str_1));
 	printf("str_2 -> %s \n", ft_strlowcase(str_2));
 	printf("str_3 -> %s \n", ft_strlowcase(str_3));
