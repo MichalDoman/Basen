e8f2a432a62718dfffd2b41c311cfeb55f9499b4
MichalDoman <mjd.domanski@gmail.com>
2025-06-24 11:55:54 +0200

    C00ex06

--- C00/ex06/ft_print_comb2.c
+++ C00/ex06/ft_print_comb2.c
@@ -5,19 +5,17 @@ void	ft_print_comb2(void)
 	int		n1;
 	int		n2;
 
-	n1 = 0;
-	while (n1 < 99)
+	n1 = -1;
+	while (++n1 < 99)
 	{
-		n2 = n1 + 1;
-		while (n2 <= 99)
+		n2 = n1;
+		while (++n2 <= 99)
 		{
 			write(1, (char []){'0' + n1 / 10, '0' + n1 % 10, ' '}, 3);
 			write(1, (char []){'0' + n2 / 10, '0' + n2 % 10}, 2);
 			if (n1 != 98)
 				write(1, ", ", 2);
-			n2++;
 		}
-		n1++;
 	}
 }
 
