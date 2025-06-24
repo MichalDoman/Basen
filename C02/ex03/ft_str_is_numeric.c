a8750395fe7e602a99d6a2095c33d6b2827100b6
MichalDoman <mjd.domanski@gmail.com>
2025-06-24 15:16:20 +0200

    C02ex03

--- C02/ex03/ft_str_is_numeric.c
+++ C02/ex03/ft_str_is_numeric.c
@@ -1,13 +1,12 @@
 int	ft_str_is_numeric(char *str)
 {
 	int	i;
-	
-	i = 0;
-	while (str[i])
+
+	i = -1;
+	while (str[++i])
 	{
 		if (!(str[i] >= '0' && str[i] <= '9'))
 			return (0);
-		i++;
 	}
 	return (1);
 }
@@ -16,10 +15,13 @@ int	ft_str_is_numeric(char *str)
 
 int	main(void)
 {
-	char str_1[] = "Is 11 numeric?";
-	char str_2[] = "12345 6";
-	char str_3[] = "123456";
+	char	*str_1;
+	char	*str_2;
+	char	*str_3;
 
+	str_1 = "Is 11 numeric?";
+	str_2 = "12345 6";
+	str_3 = "123456";
 	printf("str_1 -> %d \n", ft_str_is_numeric(str_1));
 	printf("str_2 -> %d \n", ft_str_is_numeric(str_2));
 	printf("str_3 -> %d \n", ft_str_is_numeric(str_3));
