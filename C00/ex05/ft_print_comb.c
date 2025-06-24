35531c9fac8e4c535388f9ac67b4d353b5e14475
MichalDoman <mjd.domanski@gmail.com>
2025-06-24 11:53:27 +0200

    C00ex05

--- C00/ex05/ft_print_comb.c
+++ C00/ex05/ft_print_comb.c
@@ -6,23 +6,20 @@ void	ft_print_comb(void)
 	char	j;
 	char	k;
 
-	i = '0';
-	while (i <= '7')
+	i = '/';
+	while (++i <= '7')
 	{
-		j = i + 1;
-		while (j <= '8')
+		j = i;
+		while (++j <= '8')
 		{
-			k = j + 1;
-			while (k <= '9')
+			k = j;
+			while (++k <= '9')
 			{
 				write(1, (char []){i, j, k}, 3);
 				if (i != '7')
 					write(1, ", ", 2);
-				k++;
 			}
-			j++;
 		}
-		i++;
 	}
 }
 
