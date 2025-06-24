ed1c09fc945873b398dddb80052c0e384ce554fb
MichalDoman <mjd.domanski@gmail.com>
2025-06-24 15:08:47 +0200

    C02ex01

--- C02/ex01/ft_strncpy.c
+++ C02/ex01/ft_strncpy.c
@@ -1,28 +1,28 @@
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int	i;
-	
-	i = 0;
-	while (i < n && src[i])
-	{
+	int	i;
+
+	i = -1;
+	while ((unsigned int)(++i) < n && src[i])
 		dest[i] = src[i];
-		i++;
-	}
-	while (i < n)
-	{
-		dest[i] = '\0';
-		i++;
-	}
+	while ((unsigned int)i < n)
+		dest[i++] = '\0';
 	return (dest);
 }
 
 #include <stdio.h>
+#include <string.h>
 
 int	main(void)
 {
-	char	src[] = "To be copied, however it is pretty long";
-	char	dest[50] = "Nothing here";
-	
-	printf("dest = \"%s\"", ft_strncpy(dest, src, 5));
+	char			*src;
+	char			dest[50] = "Nothing here";
+	char			dest_2[50] = "Nothing here";
+	unsigned int	n;
+
+	src = "To be copied, however it is pretty long";
+	n = 5;
+	printf("Original = \"%s\" \n", strncpy(dest, src, n));
+	printf("My function = \"%s\" \n", ft_strncpy(dest_2, src, n));
 	return (0);
 }
