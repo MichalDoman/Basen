353da9efe50cd794bce403eab637d1e63840e448
MichalDoman <mjd.domanski@gmail.com>
2025-06-24 15:18:00 +0200

    C02ex04

--- C02/ex04/ft_str_is_lowercase.c
+++ C02/ex04/ft_str_is_lowercase.c
@@ -1,13 +1,12 @@
 int	ft_str_is_lowercase(char *str)
 {
 	int	i;
-	
-	i = 0;
-	while (str[i])
+
+	i = -1;
+	while (str[++i])
 	{
 		if (!(str[i] >= 'a' && str[i] <= 'z'))
 			return (0);
-		i++;
 	}
 	return (1);
 }
@@ -16,10 +15,13 @@ int	ft_str_is_lowercase(char *str)
 
 int	main(void)
 {
-	char str_1[] = "Is it lowercase?";
-	char str_2[] = "is this lowercase?";
-	char str_3[] = "thisislowercase";
+	char	*str_1;
+	char	*str_2;
+	char	*str_3;
 
+	str_1 = "Is it lowercase?";
+	str_2 = "is this lowercase?";
+	str_3 = "thisislowercase";
 	printf("str_1 -> %d \n", ft_str_is_lowercase(str_1));
 	printf("str_2 -> %d \n", ft_str_is_lowercase(str_2));
 	printf("str_3 -> %d \n", ft_str_is_lowercase(str_3));
