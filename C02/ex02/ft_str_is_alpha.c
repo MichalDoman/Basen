1e6cb3ae0b7bf1629b623fb9a6f05fa6fff5f95b
MichalDoman <mjd.domanski@gmail.com>
2025-06-24 15:14:07 +0200

    C02ex02

--- C02/ex02/ft_str_is_alpha.c
+++ C02/ex02/ft_str_is_alpha.c
@@ -1,14 +1,13 @@
 int	ft_str_is_alpha(char *str)
 {
 	int	i;
-	
-	i = 0;
-	while (str[i])
+
+	i = -1;
+	while (str[++i])
 	{
-		if (!(str[i] >= 'a' && str[i] <= 'z') 
+		if (!(str[i] >= 'a' && str[i] <= 'z')
 			&& !(str[i] >= 'A' && str[i] <= 'Z'))
 			return (0);
-		i++;
 	}
 	return (1);
 }
@@ -17,10 +16,13 @@ int	ft_str_is_alpha(char *str)
 
 int	main(void)
 {
-	char str_1[] = "Is it alpha?";
-	char str_2[] = "Is THIS alpha1";
-	char str_3[] = "ThisIsAlpha";
+	char	*str_1;
+	char	*str_2;
+	char	*str_3;
 
+	str_1 = "Is it alpha?";
+	str_2 = "Is THIS alpha1";
+	str_3 = "ThisIsAlpha";
 	printf("str_1 -> %d \n", ft_str_is_alpha(str_1));
 	printf("str_2 -> %d \n", ft_str_is_alpha(str_2));
 	printf("str_3 -> %d \n", ft_str_is_alpha(str_3));
