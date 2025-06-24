792b15631b85c536341499ca92e734342a5a7935
MichalDoman <mjd.domanski@gmail.com>
2025-06-24 10:54:32 +0200

    C03ex04

---
+++ C03/ex04/ft_strstr.c
@@ -0,0 +1,34 @@
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	i;
+	int	j;
+
+	
+	if (!to_find[0])
+		return (str);
+	i = -1;
+	while (str[++i])
+	{
+		j = 0;
+		while (str[i + j] && str[i + j] == to_find[j])
+			j++;
+		if (!to_find[j])
+			return (str + i);
+	}
+	return (0);
+}
+
+#include <stdio.h>
+#include <string.h>
+
+int	main(void)
+{
+	char	*str;
+	char	*to_find;
+
+	str = "abbababc";
+	to_find = "ba";
+	printf("Original = %s \n", strstr(str, to_find));
+	printf("My function = %s \n", ft_strstr(str, to_find));
+	return (0);
+}
