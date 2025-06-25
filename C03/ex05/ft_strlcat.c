c0a3643fff21bad013856311276692374e776013
MichalDoman <mjd.domanski@gmail.com>
2025-06-25 14:23:57 +0200

    C03ex05

--- C03/ex05/ft_strlcat.c
+++ C03/ex05/ft_strlcat.c
@@ -1,26 +1,23 @@
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
-	unsigned int 	len;
-	unsigned int 	i;
-	unsigned int 	j;
+	unsigned int	s_len;
+	unsigned int	d_len;
+	int				i;
 
-	len = 0;
-	while (src[len])
-		len++;
-	i = 0;
-	while (dest[i])
-		i++;
-	if (size < i)
-		len += size;
-	else
-		len += i;
-	j = -1;
-	while (++j + i + 1 < size)
-		dest[i + j] = src[j];
-	dest[i + j] = '\0';
-	return (len);
+	s_len = 0;
+	while (src[s_len])
+		s_len++;
+	d_len = 0;
+	while (dest[d_len])
+		d_len++;
+	if (size <= d_len)
+		return (s_len + size);
+	i = -1;
+	while (src[++i] && i + d_len + 1 < size)
+		dest[d_len + i] = src[i];
+	return (dest[d_len + i] = '\0', s_len + d_len);
 }
-
+// i + d_len is unsigned so can be compared to size.
 #include <stdio.h>
 #include <bsd/string.h>
 
@@ -31,7 +28,7 @@ int	main(void)
 	char				dest_2[40] = "Nothing";
 	unsigned int		size;
 
-	size = 10;
+	size = 11;
 	printf("Original: %ld, %s \n", strlcat(dest_1, src, size), dest_1);
 	printf("My function: %d, %s \n", ft_strlcat(dest_2, src, size), dest_2);
 	return (0);
